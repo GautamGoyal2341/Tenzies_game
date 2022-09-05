9065a30749db0fe43185a5926e3b1a6f25783413
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-09-05 23:19:43 +0530

    button working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
 import Die from "./Die";
 function App() {
-
-   function allNewDice()
-   {
+  function allNewDice() {
     const Array = [];
-    for(let i = 0 ; i < 10 ;i++)
-    {
-        Array.push(Math.floor(Math.random()* 6));
+    for (let i = 0; i < 10; i++) {
+      Array.push(Math.floor(Math.random() * 6));
     }
     return Array;
-   }
-//    console.log(allNewDice());
+  }
+  function narray() {
+    // console.log("clicl");
+    setDice(allNewDice());
+  }
+  //    console.log(allNewDice());
 
-  const [dice,setDice] = React.useState(allNewDice());
-  const dieele = dice.map(dii => <Die value  = {dii} /> )
+  const [dice, setDice] = React.useState(allNewDice());
+  const dieele = dice.map((dii) => <Die value={dii} />);
   return (
     <div className="main">
-      <div className="dice-container">
-        {dieele}
-      </div>
-      <button className = "buttre" >RE_ROLL   </button>
+      <div className="dice-container">{dieele}</div>
+      <button className="buttre" onClick = {narray}>
+        RE_ROLL
+      </button>
     </div>
   );
 }
