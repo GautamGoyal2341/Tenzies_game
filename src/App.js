023a92211a5b120ee2f7f5d800f95bfee9fcbb60
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-09-06 22:58:22 +0530

    arr of num added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from "react";
-import Die from './Die';
-function App()
-{
-    return(
-     
-        <div className="main">
-            <div className="dice-container">
-        <Die  value = "1"/>
-        <Die  value = "2"/>
-        <Die  value = "3"/>
-        <Die  value = "4"/>
-        <Die  value = "5"/>
-        <Die  value = "6"/>
-        <Die  value = "7"/>
-        <Die  value = "1"/>
-        <Die  value = "1"/>
-        <Die  value = "1"/>
-        </div>
+import Die from "./Die";
+function App() {
 
-        </div>
+   function allNewDice()
+   {
+    const Array = [];
+    for(let i = 0 ; i < 10 ;i++)
+    {
+        Array.push(Math.floor(Math.random()* 6));
+    }
+    return Array;
+   }
+   console.log(allNewDice());
 
 
-
-    )
+  return (
+    <div className="main">
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="7" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
