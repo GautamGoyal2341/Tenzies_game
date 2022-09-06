ef0a7503aea65ceb09edf0d904660b6019eff45c
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-09-06 17:51:17 +0530

    die compo. with css added

---
+++ src/Die.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Die(prop)
+{
+    // console.log(prop)
+return <div className="sett">
+    <h1 className="die-noo" >{prop.value}</h1>
+    </div>
+}
+
+export default Die;
